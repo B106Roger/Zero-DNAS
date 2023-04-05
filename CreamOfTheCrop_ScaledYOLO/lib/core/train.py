fix wrong architecture query info; fix wrong flops calculation;

CreamOfTheCrop_ScaledYOLO/lib/core/train.py
@@ -17,6 +17,8 @@ from lib.utils.util import *
 from lib.utils.general import compute_loss, test, plot_images, is_parallel, build_foreground_mask, compute_sensitive_loss
 from lib.utils.kd_utils import compute_loss_KD
 from lib.utils.synflow import sum_arr_tensor
+from datetime import datetime
+
 
 def get_device_info():
     print('--------------GPU Info -------------------')
@@ -323,7 +325,7 @@ def training_step(task_flops, task_params, device, model, random_cand, est, wot_
 ##############################
 def train_epoch_dnas(model, dataloader, optimizer, cfg, device, task_flops, task_params, 
                      est=None, logger=None, local_rank=0, world_size=0, is_gumbel=False,
-                     prefix='', epoch=None, total_epoch=None, logdir='./', ema=None):
+                     prefix='', epoch=None, total_epoch=None, logdir='./', ema=None, warmup=True):
     batch_time_m = AverageMeter()
     data_time_m = AverageMeter()
     training_losses_m = AverageMeter()
@@ -365,7 +367,7 @@ def train_epoch_dnas(model, dataloader, optimizer, cfg, device, task_flops, task
         imgs = imgs.to(device, non_blocking=True).float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
         
         ni = iter_idx + iterations * (epoch- 1)
-        if ni <= nw:
+        if ni <= nw and warmup:
             import math
             lf = lambda x: (((1 + math.cos(x * math.pi / total_epoch)) / 2) ** 1.0) * 0.8 + 0.2
             xi = [0, nw]  # x interp
@@ -475,12 +477,204 @@ def train_epoch_dnas(model, dataloader, optimizer, cfg, device, task_flops, task
     #         #             iteration), padding=0, normalize=True)
 
 
+def train_epoch_dnas_V2(model, dataloader, theta_dataloader, optimizer, theta_optimizer, cfg, device, task_flops, task_params, 
+                     est=None, logger=None, local_rank=0, world_size=0, is_gumbel=False,
+                     prefix='', epoch=None, total_epoch=None, logdir='./', ema=None, warmup=True):
+    batch_time_m = AverageMeter()
+    data_time_m = AverageMeter()
+    training_losses_m = AverageMeter()
+    flops_losses_m = AverageMeter()
+    det_losses_m = AverageMeter()
+    
+    cache_hits = 0
+    iterations = len(dataloader)
+
+    end = time.time()
+    last_idx = len(dataloader) - 1
+    
+    batch_size = cfg.DATASET.BATCH_SIZE
+    alpha = 0.1        # for flops_loss
+    beta = 0.01         # for params_loss
+    gamma = 0.01        # for zero cost loss
+    omega = 0.01        # for depth loss
+    eta = 0.01          # for regularization loss, default 0.01
+    nw = max(3 * batch_size, 1e3)
+    is_ddp = is_parallel(model)
+    
+    # temperature = model.module.temperature if is_ddp else model.temperature
+    mloss = torch.zeros(4, device=device)  # mean losses
+    
+    pbar = enumerate(dataloader)
+    if local_rank in [-1, 0]:
+        logger.info(('%10s' * 14) % ('Epoch', 'gpu_mem', 'GIoU', 'obj', 'cls', 'total', 'targets', 'img_size', 'lr', 'moment', 'decay', 'temp', 'GFLOPS', 'f_loss'))
+        pbar = tqdm(pbar, total=iterations, bar_format='{l_bar}{bar:5}{r_bar}')  # progress bar
+    
+    def valid_generator():
+        while True:
+          for x, t, path, shape in theta_dataloader:
+            yield x, t, path, shape
+    valid_gen = valid_generator()
+    
+    temperature = model.module.temperature if is_ddp else model.temperature
+    
+    arch_theta = torch.cat([theta().reshape(1, -1) for theta in (model.module.thetas if is_ddp else model.thetas )], dim=0)
+    arch_prob = nn.functional.softmax(arch_theta, dim=-1).cpu().detach().numpy()
+    logger.info(f'Begin Architecture : {str(arch_prob)}')
+
+    for iter_idx, (imgs, targets, paths, _) in pbar:
+        ##################################################################
+        # 1st. WarmUp Learning Rate
+        ##################################################################
+        ni = iter_idx + iterations * (epoch- 1)
+        if ni <= nw and warmup:
+            import math
+            lf = lambda x: (((1 + math.cos(x * math.pi / total_epoch)) / 2) ** 1.0) * 0.8 + 0.2
+            xi = [0, nw]  # x interp
+            # model.gr = np.interp(ni, xi, [0.0, 1.0])  # giou loss ratio (obj_loss = 1.0 or giou)
+            accumulate = max(1, np.interp(ni, xi, [1, 1]).round())
+            for j, x in enumerate(optimizer.param_groups):
+                # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
+                if 'initial_lr' not in x:
+                    continue
+                w_lr = np.interp(ni, xi, [0.1 if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
+                
+                x['lr'] = w_lr
+                if 'momentum' in x:
+                    w_momentum = np.interp(ni, xi, [0.9, cfg.momentum])
+                    x['momentum'] = w_momentum
+
+
+
+        ##################################################################
+        ### 2nd. Update SuperNet Architecture
+        ##################################################################
+        if epoch > cfg.FREEZE_EPOCH:
+            # Prepare Data
+            input_valid, target_valid, _, _ = next(valid_gen)
+            # Prepare Architecture Parameter
+            train_loss, loss_items, hardware_losses, train_info = train_step_dnas(model, input_valid, target_valid, False, est, task_flops, task_params, device)
+            # squared_error_flops = hardware_losses['squared_error_flops']
+            # output_flops        = train_info['output_flops']
+            # n_targets           = train_info['n_targets']
+            # n_imgs              = train_info['n_imgs']
+
+            
+            time_grad= time.time()
+            theta_optimizer.zero_grad()
+            train_loss.backward()
+            theta_optimizer.step()
+        
+        ##################################################################
+        ### 3rd. Train SuperNet Parameter
+        ##################################################################
+        train_loss, loss_items, hardware_losses, train_info = train_step_dnas(model, imgs, targets, is_gumbel, est, task_flops, task_params, device)
+        squared_error_flops = hardware_losses['squared_error_flops']
+        output_flops        = train_info['output_flops']
+        n_targets           = train_info['n_targets']
+        n_imgs              = train_info['n_imgs']
+        if (iter_idx + 1) % 100 == 0:
+            train_prob          = train_info['architecture'].cpu().detach().numpy()
+            logger.info(f'Train Temp : {temperature} Architecture : {str(train_prob)}')
+        
+        time_grad= time.time()
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+        if ema is not None:
+            ema.update(model)
+
+        # Basic Info
+        for j, x in enumerate(optimizer.param_groups):
+            if 'momentum' in x:
+                break
+        print_lr = x['lr'] if 'lr' in x else 0
+        print_m = x['momentum'] if 'momentum' in x else 0
+        print_wdecay = x['weight_decay'] if 'weight_decay' in x else 0
+        
+        # Print
+        if local_rank in [-1, 0]:
+            ni = iter_idx
+            mloss = (mloss * iter_idx + loss_items) / (iter_idx + 1)  # update mean losses
+            mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
+            s = ('%10s' * 2 + '%10.4g' * 12) % (
+                '%g/%g' % (epoch, total_epoch), mem, *mloss, n_targets, n_imgs, print_lr, print_m, print_wdecay, temperature, output_flops, squared_error_flops.detach().cpu())
+
+            date_time = datetime.now().strftime('%m/%d %I:%M %p') + ' | '
+            pbar.set_description(date_time + s)
+            
+            # Plot
+            if ni < 3:
+                f = str(Path(logdir) / ('train_batch%g.jpg' % ni))  # filename
+                result = plot_images(images=imgs, targets=targets, paths=paths, fname=f)
+
+    arch_theta = torch.cat([theta().reshape(1, -1) for theta in (model.module.thetas if is_ddp else model.thetas )], dim=0)
+    arch_prob = nn.functional.softmax(arch_theta, dim=-1).cpu().detach().numpy()
+    logger.info(f'End Architecture : {str(arch_prob)}', )
+
+    logger.info(s)        
+    torch.cuda.synchronize()
+
+
+def train_step_dnas(model, input, targets, is_gumbel, est, task_flops, task_params, device):
+    alpha = 0.1        # for flops_loss
+    beta = 0.01         # for params_loss
+    gamma = 0.01        # for zero cost loss
+    omega = 0.01        # for depth loss
+    eta = 0.01          # for regularization loss, default 0.01
+    is_ddp = is_parallel(model)
+    temperature = model.module.temperature if is_ddp else model.temperature
+
+    # imgs = (batch=2, 3, height, width)
+    imgs = input.to(device, non_blocking=True).float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
+    arch_theta = torch.cat([theta().reshape(1, -1) for theta in (model.module.thetas if is_ddp else model.thetas )], dim=0)
+    if is_gumbel:
+        gumbel_prob = nn.functional.gumbel_softmax(arch_theta, temperature, dim=-1)
+    else:
+        gumbel_prob = nn.functional.softmax(arch_theta, dim=-1)
+    pred = model.module(imgs, gumbel_prob) if is_ddp else model(imgs, gumbel_prob)
+    
+    det_loss, loss_items = compute_loss(pred[0][1], targets.to(device), model)  # scaled by batch_size
+    det_loss = det_loss[0]
+    
+    architecture_info = {
+        'arch_type': 'continuous',
+        'arch': gumbel_prob
+    }
+    flops = model.module.calculate_flops_new(architecture_info, est.flops_dict) if is_ddp else model.calculate_flops_new(architecture_info, est.flops_dict)
+    # params = model.module.calculate_params_new(architecture_info, est.params_dict)
+    # layers = model.module.calculate_layers_new(architecture_info)
+    
+    output_flops = flops.mean() / 1e3
+    # output_params = params.mean() 
+    # output_layers = layers.mean() 
+    
+    squared_error_flops = (output_flops - task_flops) ** 2
+    # squared_error_params = (output_params - task_params) ** 2
+    
+    flops_loss = squared_error_flops * alpha
+    # params_loss = squared_error_params * beta
+    # layers_loss = output_layers
+    train_loss = det_loss + flops_loss
+
+    hardware_losses = {
+        'squared_error_flops': squared_error_flops
+    }
+    train_info = {
+        'output_flops' : output_flops,
+        'n_targets' : targets.shape[0],
+        'n_imgs'    : imgs.shape[-1],
+        'architecture' : gumbel_prob
+
+    }
+
+    return train_loss, loss_items, hardware_losses, train_info
+
 ##############################
 # Train Sensitive
 ##############################
 def train_epoch_sensitive(model, dataloader, optimizer, cfg, device, task_flops, task_params, 
                      est=None, logger=None, local_rank=0, world_size=0, is_gumbel=False,
-                     prefix='', epoch=None, total_epoch=None, logdir='./', ema=None):
+                     prefix='', epoch=None, total_epoch=None, logdir='./', ema=None, warmup=True):
     batch_time_m = AverageMeter()
     data_time_m = AverageMeter()
     training_losses_m = AverageMeter()
@@ -517,7 +711,7 @@ def train_epoch_sensitive(model, dataloader, optimizer, cfg, device, task_flops,
         # Learning WarmUp
         ########################################################
         ni = iter_idx + iterations * (epoch- 1)
-        if ni <= nw:
+        if ni <= nw and warmup:
             import math
             lf = lambda x: (((1 + math.cos(x * math.pi / total_epoch)) / 2) ** 1.0) * 0.8 + 0.2
             xi = [0, nw]  # x interp

CreamOfTheCrop_ScaledYOLO/lib/models/structures/supernet.py
@@ -303,7 +303,9 @@ class SuperNet(nn.Module):
         # Pass data through stem
         # print('Initial shape:', x.shape)
         if distributions is None:
+            # arch_theta = torch.cat([theta().reshape(1, -1) for theta in (model.module.thetas if is_ddp else model.thetas )], dim=0)
             distributions = torch.cat([theta().reshape(1, -1) for theta in self.thetas], dim=0)
+            # distributions = distributions.detach() / self.temperature
             distributions = distributions.detach()
             distributions = nn.functional.softmax(distributions, dim=-1)
         
@@ -342,10 +344,11 @@ class SuperNet(nn.Module):
                         soft_mask_variables = distributions[current_theta]
                         # print(f'{current_theta:2d} candidate layer: {soft_mask_variables.detach().cpu().numpy()}')
                         gamma_length = len(self.search_space['gamma'])
+                        depth_length = len(self.search_space['n_bottlenecks'])
                         current_operation = 0
                         operators_outputs = []
                         for depth_idx, op in enumerate(blocks):
-                            gamma_raw_dist = [soft_mask_variables[depth_idx + gamma_idx * gamma_length] for gamma_idx in range(gamma_length)]
+                            gamma_raw_dist = [soft_mask_variables[depth_idx + gamma_idx * depth_length] for gamma_idx in range(gamma_length)]
                             depth_dist = sum(gamma_raw_dist)
                             gamma_list = [gamma_prob / depth_dist for gamma_prob in gamma_raw_dist]
                             args = {

CreamOfTheCrop_ScaledYOLO/lib/utils/flops_table.py
@@ -185,9 +185,9 @@ class FlopsEst(object):
                                 
 
                     max_param_arch_params += param_list[np.argmax(param_list)] # / 1e6
-                    max_param_arch_flops  += flops_list[np.argmax(param_list)] # / 1e6
+                    max_param_arch_flops  += flops_list[np.argmax(flops_list)] # / 1e6
                     min_param_arch_params += param_list[np.argmin(param_list)] # / 1e6
-                    min_param_arch_flops  += flops_list[np.argmin(param_list)] # / 1e6
+                    min_param_arch_flops  += flops_list[np.argmin(flops_list)] # / 1e6
                     
             # Compute Head FLOPs
             print('Computing head FLOPs...')

CreamOfTheCrop_ScaledYOLO/tools/train_dmasking.py
@@ -32,7 +32,7 @@ except ImportError:
 
 # import models and training functions
 from lib.utils.flops_table import FlopsEst
-from lib.core.train import train_epoch, validate, train_epoch_dnas
+from lib.core.train import train_epoch, validate, train_epoch_dnas, train_epoch_dnas_V2
 from lib.models.structures.supernet import gen_supernet
 from lib.models.PrioritizedBoard import PrioritizedBoard
 from lib.models.MetaMatchingNetwork import MetaMatchingNetwork
@@ -54,7 +54,7 @@ def config_backup(backup_path, args):
     shutil.copy(args.hyp,  os.path.join(backup_path, os.path.basename(args.hyp)))
     shutil.copy(args.data, os.path.join(backup_path, os.path.basename(args.data)))
     shutil.copy('lib/core/train.py', os.path.join(backup_path, 'core_train.py'))
-    shutil.copy('tools/train_dnas.py', os.path.join(backup_path, 'tools_train_dnas.py'))
+    shutil.copy(f'tools/{os.path.basename(__file__)}', os.path.join(backup_path, os.path.basename(__file__)))
     shutil.copy('lib/models/structures/supernet.py', os.path.join(backup_path, 'supernet.py'))
     shutil.copy('lib/models/blocks/yolo_blocks.py', os.path.join(backup_path, 'yolo_blocks.py'))
     shutil.copy('lib/models/builders/build_supernet.py', os.path.join(backup_path, 'build_supernet.py'))
@@ -65,17 +65,22 @@ def config_backup(backup_path, args):
         f.writelines(' '.join(sys.argv))
     
 task_dict = {
-    'DNAS-25':     { 'GFLOPS': 25,  'PARAMS': 34.84, 'CHOICES': {'n_bottlenecks': [0, 1, 2], 'gamma': [0.25, 0.50, 0.75]}},
+    'DNAS-25':     { 'GFLOPS': 25,  'PARAMS': 34.84, 'CHOICES': {'n_bottlenecks': [0, 1, 2], 'gamma': [0.25, 0.50, 0.75]}},    
     'DNAS-35':     { 'GFLOPS': 35,  'PARAMS': 34.84, 'CHOICES': {'n_bottlenecks': [0, 1, 2], 'gamma': [0.25, 0.50, 0.75]}},
     'DNAS-45':     { 'GFLOPS': 45,  'PARAMS': 34.84, 'CHOICES': {'n_bottlenecks': [0, 1, 2], 'gamma': [0.25, 0.50, 0.75]}},  
     
+    'DNAS-25-No-Depth':     { 'GFLOPS': 25,  'PARAMS': 34.84, 'CHOICES': {'n_bottlenecks': [0], 'gamma': [0.25, 0.50, 0.75]}},
+
+
     'NAS-SS': { 'GFLOPS': 5.7,  'PARAMS': 32.0, 'CHOICES': {'n_bottlenecks': [0, 6, 4, 2], 'gamma': [0.25, 0.5, 0.75]}},
     'NAS-S':  { 'GFLOPS': 7.0,  'PARAMS': 36.0, 'CHOICES': {'n_bottlenecks': [0, 6, 4, 2], 'gamma': [0.25, 0.5, 0.75]}},
     'NAS-M':  { 'GFLOPS': 9.0,  'PARAMS': 40.0, 'CHOICES': {'n_bottlenecks': [8, 6, 4, 2], 'gamma': [0.25, 0.5, 0.75]}},
     'NAS':    { 'GFLOPS': 11.9, 'PARAMS': 52.5, 'CHOICES': {'n_bottlenecks': [8, 6, 4, 2], 'gamma': [0.25, 0.5, 0.75]}},
     'NAS-L':  { 'GFLOPS': 16.5, 'PARAMS': 70.2, 'CHOICES': {'n_bottlenecks': [8, 6, 4, 2], 'gamma': [0.25, 0.5, 0.75]}},
 }
-task_name = 'DNAS-35'
+# task_name = 'DNAS-25-No-Depth'
+task_name = 'DNAS-25'
+
 FLOP_RESOLUTION = None
 TASK_FLOPS      = task_dict[task_name]['GFLOPS']     # e.g TASK_FLOPS  = 5  means 50 GFLOPs
 TASK_PARAMS     = task_dict[task_name]['PARAMS']     # e.g TASK_PARAMS = 32 means 32 million parameters.
@@ -139,7 +144,7 @@ def main():
         slice=cfg.SUPERNET.SLICE,
         verbose=cfg.VERBOSE,
         logger=logger,
-        init_temp=5.0)
+        init_temp=cfg.TEMPERATURE.INIT)
     
     # print(model)
     # initialize meta matching networks
@@ -222,8 +227,6 @@ def main():
     with open(args.hyp) as f:
         hyp = yaml.load(f, Loader=yaml.FullLoader)  # load hyps
 
-    
-
     # Trainloader
     with open(args.data) as f:
         data_dict = yaml.load(f, Loader=yaml.FullLoader)  # model dict
@@ -284,36 +287,41 @@ def main():
         #     checkpoint_dir=output_dir,
         #     decreasing=decreasing)
 
+
+    #############################################################
+    # Resume Training
+    #############################################################
+    # model.load_state_dict(torch.load('./experiments/workspace/train/YOLO-DMaskingChannel-DNAS-25-V2-decay2-02/model_40.pt'))
+    # start_epoch = 40
+    # for i in range(start_epoch): lr_scheduler.step()
+
     # training scheme
-    FREEZE_EPOCH=40
+    print('Task Flop', TASK_FLOPS)
+    print('freeze epoch', cfg.FREEZE_EPOCH)
     try:
         write_thetas(output_dir, model.module.thetas_main if is_parallel(model) else model.thetas_main, -1)
         torch.save(model.state_dict(), os.path.join(output_dir, f'model_0.pt'))
-        for epoch in range(1, num_epochs+1):
+        for epoch in range(start_epoch, num_epochs+1):
             model.train()
-            thetas_enable = False if epoch <= FREEZE_EPOCH else True
+
+            train_epoch_dnas_V2(model, dataloader_weight, dataloader_thetas, optimizer, theta_optimizer, cfg, device=device, 
+                             task_flops=TASK_FLOPS, task_params=TASK_PARAMS, logger=logger, 
+                             est=model_est, local_rank=args.local_rank, world_size=args.world_size, 
+                             epoch=epoch, total_epoch=num_epochs, logdir=output_dir, is_gumbel=True, ema=ema
+                            )
+
+            thetas_enable = False if epoch <= cfg.FREEZE_EPOCH else True
             if not thetas_enable:
-                train_epoch_dnas(model, dataloader_weight, optimizer, cfg, device=device, 
-                                 task_flops=TASK_FLOPS, task_params=TASK_PARAMS, logger=logger, 
-                                 est=model_est, local_rank=args.local_rank, world_size=args.world_size, 
-                                 epoch=epoch, total_epoch=num_epochs, logdir=output_dir, is_gumbel=False, ema=ema
-                                )
-                if epoch == FREEZE_EPOCH:
+                if epoch == cfg.FREEZE_EPOCH:
                     torch.save(model.state_dict(), os.path.join(output_dir, f'model_40.pt'))
             else:
-                train_epoch_dnas(model, dataloader_weight, optimizer, cfg, device=device, 
-                                task_flops=TASK_FLOPS, task_params=TASK_PARAMS, logger=logger, 
-                                est=model_est, local_rank=args.local_rank, world_size=args.world_size, 
-                                epoch=epoch, total_epoch=num_epochs, logdir=output_dir, is_gumbel=True, ema=ema
-                                )
-                train_epoch_dnas(model, dataloader_thetas, theta_optimizer, cfg, device=device, 
-                                task_flops=TASK_FLOPS, task_params=TASK_PARAMS, logger=logger, 
-                                est=model_est, local_rank=args.local_rank, world_size=args.world_size, 
-                                epoch=epoch, total_epoch=num_epochs, logdir=output_dir, is_gumbel=True, ema=ema
-                                )
-                
                 if thetas_enable:
-                    temp_decay = np.exp(-0.065)
+                    # temp_decay = np.exp(-0.045) # 0.9560
+                    # temp_decay = 0.2**(1/80)    # 0.9800
+                    temp_decay = (0.2 * cfg.TEMPERATURE.FINAL)**(1/(num_epochs-cfg.FREEZE_EPOCH)) # 0.9560
+
+
+
                     print('Decreasing temperature!')
                     if is_parallel(model):
                         model.module.temperature = model.module.temperature * temp_decay