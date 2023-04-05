fix wrong architecture query info; fix wrong flops calculation; 2

ScaledYOLOv4/detect.py
@@ -60,7 +60,7 @@ def detect(save_img=False):
     t0 = time.time()
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-    for path, img, im0s, vid_cap in dataset:
+    for idx, (path, img, im0s, vid_cap) in enumerate(dataset):
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0

ScaledYOLOv4/utils/torch_utils.py
@@ -140,7 +140,10 @@ def model_info(model, verbose=False, resolution = (224, 224)):
         flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, resolution[0], resolution[1]),), verbose=False)[0] / 1E9 * 2
         fs = f', {flops:.1f} GFLOPS  Resolution: {resolution}'   # 640x640 FLOPS
     except:
-        fs = ''
+        # fs = ''
+        from thop import profile
+        flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, resolution[0], resolution[1]),), verbose=False)[0] / 1E9 * 2
+        fs = f', {flops:.1f} GFLOPS  Resolution: {resolution}'   # 640x640 FLOPS
 
     print('Model Summary: %g layers, %g parameters, %g gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
     return_obj = {